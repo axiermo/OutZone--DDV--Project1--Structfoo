26df7cd868c262f68fa9b007c2969cd3cfbdea4c
Pau Bonet Vall Llebrera <paubv1997@gmail.com>
2016-02-17 16:57:52 +0100

    I did my little change
    I add new line command in every printf and i coded the if part

--- main.cpp
+++ main.cpp
@@ -2,9 +2,13 @@
 #include <stdlib.h>
 
 int main(){
-	printf("Hello from the other side, I'm Daniel Garcia");
-	printf("My name is Marc de Pedro and I'm going to kill you");
-	
+	bool theprogramisworking=TRUE;
+	if(theprogramisworking){
+        printf("Hello from the other side, I'm Daniel Garcia\n");
+        printf("My name is Marc de Pedro and I'm going to kill you\n");
+        printf("I'm Pau Bonet and the one above me seem to be a little nervous\n");
+
+    }
 	system("pause");
 	return 0;
 }
