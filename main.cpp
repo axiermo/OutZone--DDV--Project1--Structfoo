c70fe05ecbf83856df215243ff6b8c4ba07ec484
JanFuentes <janffh97@gmail.com>
2016-02-17 17:39:06 +0100

    This is my change (Jan)
    The change!

--- main.cpp
+++ main.cpp
@@ -2,19 +2,16 @@
 #include <stdlib.h>
 
 int main(){
-<<<<<<< HEAD
+
 	bool theprogramisworking=TRUE;
 	if(theprogramisworking){
         printf("Hello from the other side, I'm Daniel Garcia\n");
         printf("My name is Marc de Pedro and I'm going to kill you\n");
         printf("I'm Pau Bonet and the one above me seem to be a little nervous\n");
+		printf("It's me Jan Fuentes! And I'm going to stay away from Marc\n");
 
     }
-=======
-	printf("Hello from the other side, I'm Daniel Garcia");
-	printf("Hello from Marc de Pedro. I'm going to kill you!");
-	
->>>>>>> origin/master
+
 	system("pause");
 	return 0;
 }
